d13232561bbf0e2f67fdb5dc3a326c920c5dd58e
mennamorato <mark@aviatrix.com>
2017-06-08 21:32:06 -0700

    updated conf.py for RTD theme

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+import sphinx_rtd_theme
 
 # -- General configuration ------------------------------------------------
 
@@ -47,8 +47,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'aviatrix_docs'
-copyright = '2017, Mark Ennamorato'
-author = 'Mark Ennamorato'
+copyright = '2017, Aviatrix Systems, Inc'
+author = 'Aviatrix'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -84,7 +84,8 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
