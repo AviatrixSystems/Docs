494a65c658196c408dca5cfcb4bf6f8b2262e539
Sherry Wei <sxw97g@gmail.com>
2018-03-01 21:33:15 -0800

    change theme to alabaster

--- conf.py
+++ conf.py
@@ -148,8 +148,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'aviatrix_docs.tex', 'aviatrix\\_docs Documentation',
-     'Mark Ennamorato', 'manual'),
+    (master_doc, 'aviatrix_docs.tex', 'aviatrix\\_docs Home',
+     'Aviatrix Systems', 'manual'),
 ]
 
 
