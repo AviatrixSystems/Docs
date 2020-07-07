146c20d4ecd028fce25ec9de2a818c3aa2db14c9
Sherry Wei <sxw97g@gmail.com>
2020-07-06 18:23:56 -0700

    remove disqus extension

--- conf.py
+++ conf.py
@@ -40,9 +40,9 @@ def setup(app):
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-#extensions = []
+extensions = []
 
-# extensions = ['sphinxcontrib.disqus']
+#extensions = ['sphinxcontrib.disqus']
 
 
 # Add any paths that contain templates here, relative to this directory.
