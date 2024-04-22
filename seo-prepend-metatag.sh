61efac94f8328efb37636a16c2acdaa63af73407
Alice Liu <104796095+aliceliudoc@users.noreply.github.com>
2024-04-22 17:20:57 -0400

    Update seo-prepend-metatag.sh

--- seo-prepend-metatag.sh
+++ seo-prepend-metatag.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# Define the directory containing the .rst files
 directory="."
 
-# Define the meta content to add
 meta_content=".. raw:: html\n\n    <meta name=\"robots\" content=\"noindex, nofollow, noarchive, nosnippet, notranslate, noimageindex\">\n"
 
-# Find all .rst files in the directory and its subdirectories
 find "$directory" -name "*.rst" -type f | while read file; do
-    # Check if the meta tag is already in the file
     if ! grep -Fq "raw:: html" "$file"; then
         # Prepend the meta content to each file
         echo -e "$meta_content$(cat "$file")" > "$file"
