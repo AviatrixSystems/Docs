db8cd183d1da7dfbbe6e75583147a0f32c0c2094
Alice Liu <104796095+aliceliudoc@users.noreply.github.com>
2024-10-06 23:09:00 -0400

    Main copy (#1217)
    * add azure account audit information
    
    * add GCP account audit
    
    * CVE-2023-52087
    
    * Update PSIRT_Advisories.rst
    
    * Update PSIRT_Advisories.rst
    
    * Update PSIRT_Advisories.rst
    
    * CVE-2023-52087
    
    * CVE-2023-52087
    
    * Update push-external.yml
    
    * Update README.md
    
    * Update push-external.yml
    
    * Update README.md
    
    * Update README.md
    
    * Update push-external.yml
    
    * ci: token
    
    * ci: fix checkout
    
    * ci: fix
    
    * ci: fix
    
    * ci: fix
    
    * debug: temp
    
    * ci: fix
    
    * debug: rm
    
    * ci: test
    
    * ci: fix push
    
    * ci: fix names
    
    * ci: perms
    
    * ci: push
    
    * ci: set-url
    
    * ci: add remote
    
    * ci: fix
    
    * ci: checkout
    
    * ci: test
    
    * ci: fix name
    
    * ci: fix push cmd
    
    * ci: config
    
    * ci: push with token
    
    * ci: add username
    
    * Remove the CloudFormation deployment page.
    
    * ci: use all perms
    
    * Update push-external.yml
    
    * Update push-external.yml
    
    * Update .gitignore
    
    * Update push-external.yml
    
    * Update push-external.yml
    
    Try to push directly to main
    
    * Update push-external.yml
    
    * add block google SEO metatag
    
    * add script to stop google search engine from indexing
    
    * stop all the web crawlers from crawling this repo
    
    * minor update
    
    * Egress FQDN deprecation page.
    
    * Egress FQDN deprecation page.
    
    ---------
    
    Co-authored-by: Shamilton42 <81544010+Shamilton42@users.noreply.github.com>
    Co-authored-by: Rodman Stuhlmuller <rodmans@aviatrix.com>

--- seo-prepend-metatag.sh
+++ seo-prepend-metatag.sh
@@ -5,6 +5,7 @@ directory="."
 meta_content=".. raw:: html\n\n    <meta name=\"robots\" content=\"noindex, nofollow, noarchive, nosnippet, notranslate, noimageindex\">\n"
 
 find "$directory" -name "*.rst" -type f | while read file; do
+
     if ! grep -Fq "raw:: html" "$file"; then
         # Prepend the meta content to each file
         echo -e "$meta_content$(cat "$file")" > "$file"
